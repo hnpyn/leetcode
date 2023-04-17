ead2236c085d04f8acafd91653802aba1fbf8811
hnpyn <wangmingne@outlook.com>
2023-04-17 22:50:30 +0800

    update

--- hash_table/242.有效的字母异位词.cpp
+++ hash_table/242.有效的字母异位词.cpp
@@ -9,3 +9,33 @@
 
 说明: 你可以假设字符串只包含小写字母。
 */
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // 方法一，先排序，然后直接比较，省略
+        // 方法二，哈希表
+        if (s.length()!= t.length()) {
+            return false;
+        }
+        // 数组作为一个简单的哈希表，下标即为key
+        vector<int> record(26, 0);
+        for (auto ch: s) {
+            // 减去'a'，将ASCII码映射到 0-25，对应数组下标
+            record[ch - 'a']++;
+        }
+        for (auto ch: t) {
+            record[ch - 'a']--;
+            // 不是字母异位词，必然会出现小于0的情况
+            if (record[ch - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
