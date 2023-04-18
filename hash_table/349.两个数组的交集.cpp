3801789b5d67a1fe3416ae4758d448100a807434
hnpyn <wangmingne@outlook.com>
2023-04-18 23:17:36 +0800

    update

--- hash_table/349.两个数组的交集.cpp
+++ hash_table/349.两个数组的交集.cpp
@@ -4,3 +4,22 @@
 说明： 输出结果中的每个元素一定是唯一的。 我们可以不考虑输出结果的顺序。
 */
 
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> result_set; // 存放结果，之所以用set是为了给结果集去重
+        unordered_set<int> nums_set(nums1.begin(), nums1.end());
+        for (int num : nums2) {
+            // 发现nums2的元素 在nums_set里又出现过
+            if (nums_set.find(num) != nums_set.end()) {
+                result_set.insert(num);
+            }
+        }
+        return vector<int>(result_set.begin(), result_set.end());
+    }
+};
