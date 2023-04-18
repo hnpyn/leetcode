21ae594ae0dd52b71d4d49a94960f36822b023a1
hnpyn <wangmingne@outlook.com>
2023-04-19 00:43:05 +0800

    update

--- hash_table/349.两个数组的交集.cpp
+++ hash_table/349.两个数组的交集.cpp
@@ -12,10 +12,10 @@ using namespace std;
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> result_set; // 存放结果，之所以用set是为了给结果集去重
+        unordered_set<int> result_set;  // 存放结果，之所以用set是为了给结果集去重
         unordered_set<int> nums_set(nums1.begin(), nums1.end());
         for (int num : nums2) {
-            // 发现nums2的元素 在nums_set里又出现过
+            // 发现nums2的元素，在nums_set里又出现过
             if (nums_set.find(num) != nums_set.end()) {
                 result_set.insert(num);
             }
