82bd35b0efdcaef5a33f149a56ee5f483799d905
hnpyn <wangmingne@outlook.com>
2023-06-27 01:12:20 +0800

    update string

--- string/459.重复的子字符串.cpp
+++ string/459.重复的子字符串.cpp
@@ -20,3 +20,37 @@
 
 using namespace std;
 
+// KMP 算法
+class Solution {
+public:
+    void getNext(int* next, const string& s) {
+        int j = 0;
+        next[0] = j;
+        for (int i = 1; i < s.size(); ++i) {
+            while(j > 0 && s[j] != s[i]) {
+                j = next[j - 1];
+            }
+            if (s[j] == s[i]) {
+                ++j;
+            }
+            next[i] = j;
+        }
+    }
+    bool repeatedSubstringPattern(string s) {
+        if (s.size() == 1) {
+            return false;
+        }
+        int next[s.size()];
+        getNext(next, s);
+        int len = s.size();
+        /* 
+        字符串长度减去最长相同前后缀的长度（当字符串是由重复子串构成的时候，
+        next数组的最后一位必然是最长相同前后缀长度）相当于是一个周期的长度，
+        如果这个周期可以被整除，就说明整个字符串就是这个周期的循环。
+        */
+        if (next[len - 1] > 0 && len % (len - next[len - 1]) == 0) {
+            return true;
+        }
+        return false;
+    }
+};
