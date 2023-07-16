a9f85a41169562b1ff2b02391416ec40ec125d8a
hnpyn <wangmingne@outlook.com>
2023-07-17 01:22:42 +0800

    update dual pointers

--- dual_pointers/160.链表相交.cpp
+++ dual_pointers/160.链表相交.cpp
@@ -7,10 +7,58 @@ null。
 注意，函数返回结果后，链表必须保持其原始结构。
 */
 
+#include <iostream>
+
+using namespace std;
+
 struct ListNode {
   int val;
   ListNode *next;
   ListNode() : val(0), next(nullptr) {}
   ListNode(int x) : val(x), next(nullptr) {}
   ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+  ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    if (headA == nullptr || headA == nullptr) {
+      return NULL;
+    }
+
+    ListNode *curA = headA;
+    int headALen = 0;
+    while (curA != nullptr) {
+      ++headALen;
+      curA = curA->next;
+    }
+
+    ListNode *curB = headB;
+    int headBLen = 0;
+    while (curB != nullptr) {
+      ++headBLen;
+      curB = curB->next;
+    }
+
+    curA = headA;
+    curB = headB;
+    if (headALen < headBLen) {
+      swap(headA, headB);
+      swap(curA, curB);
+    }
+    int diff = abs(headALen - headBLen);
+    for (int i = 0; i < diff; ++i) {
+      curA = curA->next;
+    }
+
+    while (curA != nullptr) {
+      if (curA == curB) {
+        return curA;
+      }
+      curA = curA->next;
+      curB = curB->next;
+    }
+
+    return NULL;
+  }
 };
